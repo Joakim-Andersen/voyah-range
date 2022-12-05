725a831fb78a1b987a46b5c155d5d79a324fd934
Joakim Andersen <80250636+Joakim-Andersen@users.noreply.github.com>
2022-12-05 14:58:16 +0100

    init
    init

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// window.addEventListener('DOMContentLoaded', (event) => {
+// 	const testDriveForm = document.getElementById('test-drive-form');
+// 	if(testDriveForm){
+
+// 		console.log('imme');
+	
+// 	}
+// });
